b835102defd162969db95f05bf04786d0b3d0ca3
Smilefounder <huy.nguyen@smileway.org>
2017-07-05 14:57:37 +0700

    Update code base

--- src/Swastika.Identity/Identity/Const.cs
+++ src/Swastika.Identity/Identity/Const.cs
@@ -10,7 +10,7 @@ namespace Swastika.Identity.Identity
         /// <summary>
         /// The constant default connection
         /// </summary>
-        public const string CONST_DEFAULT_CONNECTION = "IdentityConnection";
+        public const string CONST_DEFAULT_CONNECTION = "DefaultConnection";
 
         /// <summary>
         /// The constant file appsetting
